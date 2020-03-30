1838a2ca6f92d5a711693bbc8028a44fad8381e6
tanlei@gitlab.zhulong.com <tanlei@gitlab.zhulong.com>
2020-03-30 16:56:38 +0800

    edit

--- git_python.py
+++ git_python.py
@@ -1,7 +1,12 @@
 # -- coding: utf-8 --
 from git import Repo
+from git import Git
 repo = Repo(r'D:/htdocs/online-crm')
 # 获取默认版本库 origin
 remote = repo.remote()
 # 从远程版本库拉取分支
 remote.pull()
+
+r = Git("D:/htdocs/online-crm")
+r.execute('git pull') 
+print(r.execute('git pull'))
