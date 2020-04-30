918032b68a223fb28556229d0b8d0530490c3cc9
tanlei@gitlab.zhulong.com <tanlei@gitlab.zhulong.com>
2020-04-30 11:20:28 +0800

    edit

--- wall.py
+++ wall.py
@@ -18,7 +18,7 @@ def getTheMonth(date, n):
 date = datetime.datetime.today()
 lastmonth = getTheMonth(date, 1).strftime("%Y-%m")
 driver = webdriver.Chrome()
-driver.get('http://walle.zhulong.com:8484/login')
+driver.get('http://walle.zhulong.dj:8484/login')
 time.sleep(1);
 inputs = driver.find_elements_by_tag_name("input")
 for i in inputs:
@@ -28,7 +28,7 @@ for i in inputs:
     	i.send_keys('Zhulong123')
 driver.find_element_by_tag_name('button').click()
 time.sleep(1);
-driver.get('http://walle.zhulong.com:8484/%E7%AD%91%E9%BE%99%E5%AD%A6%E7%A4%BE/task/create/60')
+driver.get('http://walle.zhulong.dj:8484/%E7%AD%91%E9%BE%99%E5%AD%A6%E7%A4%BE/task/create/59')
 time.sleep(2);
 #driver.find_elements_by_tag_name("input").send_keys('111')
 inputs = driver.find_elements_by_tag_name("input")
@@ -37,13 +37,13 @@ for i in inputs:
     	i.click();
     	time.sleep(1);
     	el=driver.find_elements_by_class_name("el-select-dropdown__item");
-    	text=el[1].find_element_by_tag_name("span").text
+    	text=el[0].find_element_by_tag_name("span").text
     	driver.find_elements_by_class_name("el-input__inner")[0].send_keys(text)
-    	el[1].find_element_by_tag_name("span").click()
+    	el[0].find_element_by_tag_name("span").click()
     	break
 driver.find_elements_by_class_name("el-button")[1].click()
 time.sleep(2);
-driver.get('http://walle.zhulong.com:8484/%E7%AD%91%E9%BE%99%E5%AD%A6%E7%A4%BE/deploy/index')
+driver.get('http://walle.zhulong.dj:8484/%E7%AD%91%E9%BE%99%E5%AD%A6%E7%A4%BE/deploy/index')
 time.sleep(2);
 buttons=driver.find_elements_by_class_name("el-button--text")
 for i in buttons:
